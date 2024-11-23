ce877ffff658f0afeb8b53652ae85e15123eb881
Sadanand Patil <sadanand.ptl@gmail.com>
2024-11-23 09:20:55 +0000

    updated id from customers

--- models/customers.sql
+++ models/customers.sql
@@ -2,7 +2,7 @@
 
 with customer_orders as (
     select
-    c.customer_id,c.first_name,c.last_name
+    c.id,c.first_name,c.last_name
     ,min(o.order_date) as first_order,
     max(o.order_date) as most_recent_order
     ,count(o.id) as number_of_orders
@@ -10,8 +10,8 @@ with customer_orders as (
     {{ source('raw','raw_customers') }} c 
     left join
     {{ source('raw','raw_orders') }} o 
-    on c.customer_id = o.user_id
-    group by c.customer_id,c.first_name,c.last_name
+    on c.id = o.user_id
+    group by c.id,c.first_name,c.last_name
 ),
 customer_payments as (
     select o.user_id as customer_id,
@@ -24,9 +24,9 @@ customer_payments as (
     group by o.user_id
 )
 select 
-co.customer_id,co.first_name,co.last_name,co.first_order
+co.id,co.first_name,co.last_name,co.first_order
 ,co.most_recent_order,co.number_of_orders,cp.customer_lifetime_value
 from customer_orders co
 left join
 customer_payments cp
-on co.customer_id = cp.customer_id
+on co.id = cp.customer_id
